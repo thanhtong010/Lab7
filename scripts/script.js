fc5cbb9dd1074353e1eb2609b5a84408a7b346fc
thanhtong010 <72485537+thanhtong010@users.noreply.github.com>
2021-06-04 20:39:21 -0700

    Cache

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,11 @@
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 
-var CACHE_NAME = 'cache-journal';
-var urlsToCache = ['https://cse110lab6.herokuapp.com/entries'];
-
 // Make sure you register your service worker here too 
 // Register service worker (from https://developers.google.com/web/fundamentals/primers/service-workers)
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
@@ -19,56 +16,6 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
-
-// Open cache and cache files (from https://developers.google.com/web/fundamentals/primers/service-workers#cache_and_return_requests)
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
 
 // On-click event listener for clicking on settings
 document.querySelector('img').addEventListener('click', function() {
--- sw.js
+++ sw.js
@@ -4,3 +4,59 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = ['https://cse110lab6.herokuapp.com/entries'];
+
+// Install cache (from https://developers.google.com/web/fundamentals/primers/service-workers#cache_and_return_requests)
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+  
+// Activating service worker (from https://developer.mozilla.org/en-US/docs/Web/API/Clients/claim)
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+// Fetch requests (from https://developers.google.com/web/fundamentals/primers/service-workers)
+self.addEventListener('fetch', function(event) {
+    cache('fetching')
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
